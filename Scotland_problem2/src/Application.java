aa4b1dfde6a2943c3797506d89d89dc371e94bce
sy446392 <59713310+sy446392@users.noreply.github.com>
2020-03-29 19:10:16 -0400

    Add files via upload

---
+++ Scotland_problem2/src/Application.java
@@ -0,0 +1,13 @@
+public class Application {
+  
+  
+public static void main(String[] args)
+{
+DuplicateCounter dc = new DuplicateCounter();
+
+
+
+dc.count("problem2.txt");
+dc.write("unique_word_counts.txt");
+}
+}
